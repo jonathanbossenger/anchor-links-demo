bae935ca7edde8d9c62833bc7b84639bee5ee8be
Jonathan Bossenger <jonathanbossenger@gmail.com>
2017-02-08 14:07:20 +0200

    Adding Readme details

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Anchor Links Demo
+#Anchor Links Demo
 Divi anchor links demo - opening tabs and accordions from another page
+
+##Usage
+1. Install the Divi theme
+1. Install and activate the Anchor Links Demo child theme
+1. Create a new page and import the Anchor Links Demo.json file for the page layout
+1. Create links to this new page using the following format:
+    1. http://url.com/new-page-slug/?et_open_accordion=et_pb_accordion_item_1    
+    1. http://url.com/new-page-slug/?et_open_tab=et_pb_tab_1
+     
+See this article for details on how everything works.
+
+https://jonathanbossenger.com/using-anchor-links-to-open-accordions-and-tabs-in-divi-from-another-page/ 
--- functions.js
+++ functions.js
@@ -1,21 +1,16 @@
 jQuery(function ($) {
-
     $( document ).ready(function() {
-
         // accordion
         if (typeof openers.accordion !== 'undefined') {
             var accordion_element = '#my-accordian .' + openers.accordion + ' h5.et_pb_toggle_title';
             $(accordion_element).click();
             $("html, body").animate({ scrollTop: $('#my-accordian').offset().top }, 1000);
         }
-
         // tab
         if (typeof openers.tab !== 'undefined') {
             var tab_element = '#my-tabs .' + openers.tab + ' a';
             $(tab_element).click();
             $("html, body").animate({ scrollTop: $('#my-tabs').offset().top }, 1000);
         }
-
     });
-
 });
